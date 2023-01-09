161bbd874274a4ec42e4db13a439aa37f42a7204
FikranAkbar <fikranakbar756@gmail.com>
2023-01-09 20:42:19 +0700

    add one to many relationship of master user and trx user like

--- model/entity/trx_user_like.go
+++ model/entity/trx_user_like.go
@@ -6,10 +6,12 @@ import (
 
 type TrxUserLike struct {
 	gorm.Model     `gorm:"embedded"`
-	LikeableId     uint `gorm:"column:likeable_id;not null"`
-	LikeableTypeId uint `gorm:"column:likeable_type_id;not null"`
-	UserId         uint `gorm:"column:user_id;not null"`
-	IsLike         bool `gorm:"column:is_like;default:false"`
+	LikeableId     uint               `gorm:"column:likeable_id;not null"`
+	LikeableTypeId uint               `gorm:"column:likeable_type_id;not null"`
+	LikeableType   MasterLikeableType `gorm:"foreignKey:LikeableTypeId;joinForeignKey:ID"`
+	UserId         uint               `gorm:"column:user_id;not null"`
+	User           MasterUser         `gorm:"foreignKey:UserId;joinForeignKey:ID"`
+	IsLike         bool               `gorm:"column:is_like;default:false"`
 }
 
 func (TrxUserLike) TableName() string {
