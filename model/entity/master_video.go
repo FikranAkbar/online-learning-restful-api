9b1ff649429d951ded574a9dabf829a04a906336
FikranAkbar <fikranakbar756@gmail.com>
2023-01-09 20:29:21 +0700

    add one to one relationship of master elearning module and master video

--- model/entity/master_video.go
+++ model/entity/master_video.go
@@ -5,14 +5,12 @@ import (
 )
 
 type MasterVideo struct {
-	gorm.Model            `gorm:"embedded"`
-	CourseId              uint                  `gorm:"column:course_id;not null"`
-	MasterCourse          MasterCourse          `gorm:"foreignKey:CourseId"`
-	ModuleId              uint                  `gorm:"column:module_id;not null"`
-	MasterElearningModule MasterElearningModule `gorm:"foreignKey:ModuleId"`
-	VideoName             string                `gorm:"column:video_name;type:varchar(100);not null;unique_index"`
-	VideoUrl              string                `gorm:"column:video_url;not null"`
-	IsPublished           bool                  `gorm:"column:is_published;default:false"`
+	gorm.Model  `gorm:"embedded"`
+	CourseId    uint   `gorm:"column:course_id;not null"`
+	ModuleId    uint   `gorm:"column:module_id;not null"`
+	VideoName   string `gorm:"column:video_name;type:varchar(100);not null;unique_index"`
+	VideoUrl    string `gorm:"column:video_url;not null"`
+	IsPublished bool   `gorm:"column:is_published;default:false"`
 }
 
 func (MasterVideo) TableName() string {
