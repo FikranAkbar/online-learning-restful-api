73be23bbdad7c091d0705a58bddb827a4fcb66d6
FikranAkbar <fikranakbar756@gmail.com>
2023-01-09 21:13:21 +0700

    add one to one relationship to master city and master province

--- model/entity/master_city.go
+++ model/entity/master_city.go
@@ -6,13 +6,13 @@ import (
 )
 
 type MasterCity struct {
-	ID             *uint `gorm:"primaryKey"`
-	CreatedAt      time.Time
-	UpdatedAt      time.Time
-	DeletedAt      gorm.DeletedAt `gorm:"index"`
-	CityName       string         `gorm:"column:city_name;type:varchar(100);not null"`
-	ProvinceId     uint           `gorm:"column:province_id"`
-	MasterProvince MasterProvince `gorm:"foreignKey:ProvinceId;references:ID"`
+	ID         *uint `gorm:"primaryKey"`
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+	DeletedAt  gorm.DeletedAt `gorm:"index"`
+	CityName   string         `gorm:"column:city_name;type:varchar(100);not null"`
+	ProvinceId uint           `gorm:"column:province_id"`
+	Province   MasterProvince `gorm:"foreignKey:ProvinceId;references:ID"`
 }
 
 func (MasterCity) TableName() string {
