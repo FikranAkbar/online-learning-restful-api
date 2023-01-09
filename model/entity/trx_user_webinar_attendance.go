c44184d3abe4ed920553082d5ca43d464d401910
FikranAkbar <fikranakbar756@gmail.com>
2023-01-09 21:12:38 +0700

    add relatioship in trx user webinar attendance model

--- model/entity/trx_user_webinar_attendance.go
+++ model/entity/trx_user_webinar_attendance.go
@@ -7,11 +7,13 @@ import (
 
 type TrxUserWebinarAttendance struct {
 	gorm.Model       `gorm:"embedded"`
-	WebinarSessionId uint      `gorm:"column:webinar_session_id;not null"`
-	UserId           uint      `gorm:"column:user_id;not null"`
-	FirstJoinTime    time.Time `gorm:"column:first_join_time"`
-	LastLeaveTime    time.Time `gorm:"column:last_leave_time"`
-	JoinDuration     uint      `gorm:"column:join_duration"`
+	WebinarSessionId uint                 `gorm:"column:webinar_session_id;not null"`
+	WebinarSession   MasterWebinarSession `gorm:"foreignKey:WebinarSessionId;joinForeignKey:ID"`
+	UserId           uint                 `gorm:"column:user_id;not null"`
+	User             MasterUser           `gorm:"foreignKey:UserId;joinForeignKey:ID"`
+	FirstJoinTime    time.Time            `gorm:"column:first_join_time"`
+	LastLeaveTime    time.Time            `gorm:"column:last_leave_time"`
+	JoinDuration     uint                 `gorm:"column:join_duration"`
 }
 
 func (TrxUserWebinarAttendance) TableName() string {
