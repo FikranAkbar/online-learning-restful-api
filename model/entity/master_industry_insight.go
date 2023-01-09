88c365a68f58851ff3aca1f243d1a4eadc6d4f86
FikranAkbar <fikranakbar756@gmail.com>
2023-01-09 20:17:21 +0700

    add many to many relationship of master industry insight and master insight attachment

--- model/entity/master_industry_insight.go
+++ model/entity/master_industry_insight.go
@@ -7,12 +7,13 @@ import (
 
 type MasterIndustryInsight struct {
 	gorm.Model   `gorm:"embedded"`
-	UserAuthorId uint           `gorm:"column:user_author_id;not null"`
-	MasterUser   MasterUser     `gorm:"foreignKey:UserAuthorId;joinForeignKey:ID"`
-	TitleInsight string         `gorm:"column:title_insight;type:varchar(100);not null"`
-	CoverInsight sql.NullString `gorm:"column:cover_insight"`
-	BodyContent  sql.NullString `gorm:"column:body_content"`
-	IsPublished  bool           `gorm:"column:is_published;default:false"`
+	UserAuthorId uint                      `gorm:"column:user_author_id;not null"`
+	MasterUser   MasterUser                `gorm:"foreignKey:UserAuthorId;joinForeignKey:ID"`
+	TitleInsight string                    `gorm:"column:title_insight;type:varchar(100);not null"`
+	CoverInsight sql.NullString            `gorm:"column:cover_insight"`
+	BodyContent  sql.NullString            `gorm:"column:body_content"`
+	IsPublished  bool                      `gorm:"column:is_published;default:false"`
+	Attachments  []MasterInsightAttachment `gorm:"many2many:trx_industry_insight_attachment;foreignKey:ID;joinForeignKey:IndustryInsightId;references:ID;joinForeignKey:InsightAttachmentId"`
 }
 
 func (MasterIndustryInsight) TableName() string {
--- model/entity/trx_industry_insight_attachment.go
+++ model/entity/trx_industry_insight_attachment.go
@@ -2,14 +2,15 @@ package entity
 
 import (
 	"gorm.io/gorm"
+	"time"
 )
 
 type TrxIndustryInsightAttachment struct {
-	gorm.Model              `gorm:"embedded"`
-	IndustryInsightId       uint                    `gorm:"column:industry_insight_id;not null"`
-	MasterIndustryInsight   MasterIndustryInsight   `gorm:"foreignKey:IndustryInsightId"`
-	InsightAttachmentId     uint                    `gorm:"column:insight_attachment_id;not null"`
-	MasterInsightAttachment MasterInsightAttachment `gorm:"foreignKey:InsightAttachmentId"`
+	IndustryInsightId   uint `gorm:"column:industry_insight_id;primaryKey"`
+	InsightAttachmentId uint `gorm:"column:insight_attachment_id;primaryKey"`
+	CreatedAt           time.Time
+	UpdatedAt           time.Time
+	DeletedAt           gorm.DeletedAt `gorm:"index"`
 }
 
 func (TrxIndustryInsightAttachment) TableName() string {
