47aeb1f94218dd9fee87dfa649a1d24f07c81b8f
FikranAkbar <fikranakbar756@gmail.com>
2023-01-14 20:19:21 +0700

    add api name in login api route

--- app/router/router.go
+++ app/router/router.go
@@ -8,5 +8,5 @@ import (
 func InitRoutes(controller authentication_controller.AuthenticationController, e *echo.Echo) {
 	apiGroup := e.Group("/api")
 	usersRouteGroup := apiGroup.Group("/users")
-	usersRouteGroup.POST("/login", controller.LoginUserWithEmailPassword)
+	usersRouteGroup.POST("/login", controller.LoginUserWithEmailPassword).Name = "Login with email and password"
 }
