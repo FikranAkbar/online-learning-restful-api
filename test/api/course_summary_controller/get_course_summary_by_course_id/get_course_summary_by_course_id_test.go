6874f36e86283fe75e190269a0ac61c8ee1c2e44
FikranAkbar <fikranakbar756@gmail.com>
2023-01-23 05:15:37 +0700

    finish create testing for get course summary by course id

---
+++ test/api/course_summary_controller/get_course_summary_by_course_id/get_course_summary_by_course_id_test.go
@@ -0,0 +1,118 @@
+package get_course_summary_by_course_id
+
+import (
+	"encoding/json"
+	"github.com/mitchellh/mapstructure"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"online-learning-restful-api/app/router"
+	"online-learning-restful-api/di"
+	"online-learning-restful-api/model/web"
+	"online-learning-restful-api/model/web/course"
+	"online-learning-restful-api/test"
+	"os"
+	"regexp"
+	"testing"
+)
+
+var currentJWTToken = ""
+
+func TestMain(m *testing.M) {
+	currentJWTToken = test.LoginUserFirst(`{"email":"mollypotts@gmail.com","password":"Password"}`)
+	code := m.Run()
+	os.Exit(code)
+}
+
+func TestGetCourseSummaryByCourseIdSuccess(t *testing.T) {
+	e := di.InitializedEchoServerForTest()
+
+	courseIdPath := "/1"
+	urlRoute := router.HostURLTest +
+		router.CourseURLPath +
+		courseIdPath +
+		router.SummaryURLPath
+	req := httptest.NewRequest(
+		http.MethodGet,
+		urlRoute,
+		nil)
+	req.Header.Set("Authorization", "Bearer "+currentJWTToken)
+	rec := httptest.NewRecorder()
+
+	e.ServeHTTP(rec, req)
+
+	response := rec.Result()
+	assert.Equal(t, http.StatusOK, rec.Code)
+
+	body, _ := io.ReadAll(response.Body)
+	var responseBody web.APIResponse
+	var courseSummaryResponse course.SummaryResponse
+
+	_ = json.Unmarshal(body, &responseBody)
+	assert.Equal(t, http.StatusOK, responseBody.Status)
+	assert.Equal(t, test.MessageOk, responseBody.Message)
+	assert.NotNil(t, responseBody.Data)
+
+	_ = mapstructure.Decode(responseBody.Data, &courseSummaryResponse)
+	assert.NotNil(t, courseSummaryResponse.Id)
+	assert.NotNil(t, courseSummaryResponse.CourseId)
+	assert.NotNil(t, courseSummaryResponse.DocURL)
+}
+
+func TestGetCourseSummaryByCourseIdFailedInvalidToken(t *testing.T) {
+	e := di.InitializedEchoServerForTest()
+
+	courseIdPath := "/1"
+	urlRoute := router.HostURLTest +
+		router.CourseURLPath +
+		courseIdPath +
+		router.SummaryURLPath
+	req := httptest.NewRequest(
+		http.MethodGet,
+		urlRoute,
+		nil)
+	rec := httptest.NewRecorder()
+
+	e.ServeHTTP(rec, req)
+
+	response := rec.Result()
+	assert.Equal(t, http.StatusUnauthorized, rec.Code)
+
+	body, _ := io.ReadAll(response.Body)
+	var responseBody web.APIResponse
+
+	_ = json.Unmarshal(body, &responseBody)
+	assert.Equal(t, http.StatusUnauthorized, responseBody.Status)
+	assert.Equal(t, test.MessageUnauthorized, responseBody.Message)
+	assert.Regexp(t, regexp.MustCompile(`(?i)invalid token`), responseBody.Data)
+}
+
+func TestGetCourseSummaryByCourseIdFailedCourseNotFound(t *testing.T) {
+	e := di.InitializedEchoServerForTest()
+
+	courseIdPath := "/101"
+	urlRoute := router.HostURLTest +
+		router.CourseURLPath +
+		courseIdPath +
+		router.SummaryURLPath
+	req := httptest.NewRequest(
+		http.MethodGet,
+		urlRoute,
+		nil)
+	req.Header.Set("Authorization", "Bearer "+currentJWTToken)
+	rec := httptest.NewRecorder()
+
+	e.ServeHTTP(rec, req)
+
+	response := rec.Result()
+	assert.Equal(t, http.StatusNotFound, rec.Code)
+
+	body, _ := io.ReadAll(response.Body)
+	var responseBody web.APIResponse
+
+	_ = json.Unmarshal(body, &responseBody)
+	assert.Equal(t, http.StatusNotFound, responseBody.Status)
+	assert.Equal(t, test.MessageNotFound, responseBody.Message)
+	assert.Regexp(t, regexp.MustCompile(`(?i)not found`), responseBody.Data)
+}
